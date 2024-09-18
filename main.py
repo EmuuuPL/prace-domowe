3fb423a0f89a713c27d6bd75d767ce56c45bba5a
Emil <166171933+EmuuuPL@users.noreply.github.com>
2024-09-18 20:42:40 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("jesli chcesz sprawdzic trojkat wybierz '1' jesli kwadratu lub prostokata wybierz '2'")
+print("jesli chcesz sprawdzic trojkat wybierz '1' jesli kwadratu lub prostokatu albo czworokata wybierz '2'")
 wybor = input()
 
 if wybor == '1':
@@ -30,24 +30,24 @@ if wybor == '1':
     else:
         print("Nie jest to trojkat")    
 elif wybor == '2':
-    print("kwadrat lub prostokat")
+    print("kwadrat lub prostokat albo inny czworokat")
     a = float(input())
     b = float(input())
     c = float(input())
     d = float(input())
     
-    if a + b + c + d:
-        print("to jest kwadrat lub prostokat")
+    if ((a > 0) + (b > 0) + (c > 0) + (d > 0)):
+        print("to jest kwadrat lub prostokat albo inny czworokat")
         if a == b == c == d:
             print("to jest kwadrat")
         else:
             print("to nie jest kwadrat")
-            if a == b + c == d or a == c + b == d or a == d + b == c or b == c + a == d or b == d + a == c or c == d + a == d:
+            if a == b and c == d or a == c and b == d or a == d and b == c or b == c and a == d or b == d and a == c or c == d and a == d:
                 print("to jest prostokat")
             else:
                 print("to nie jest prostokat")
     else:
-        print("to nie jest kwadrat lub prostokat")
+        print("to nie jest kwadrat lub prostokat albo inny czworokat")
 else:
     print("zly wybor")
         
